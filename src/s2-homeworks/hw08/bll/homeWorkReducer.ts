725b3c07ae58701894d625b184ecb7a9c62873ea
Mikita Sobaleu <nick-99@tut.by>
2026-06-12 15:26:56 +0300

    finish homework 8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,19 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
-        case 'sort': { // by name
+        case 'sort': {
+            const copyState = [...state]
 
-            return state // need to fix
+            if (action.payload === 'up') {
+                return copyState.sort((a, b) => a.name.localeCompare(b.name))
+            }
+
+            return copyState.sort((a, b) => b.name.localeCompare(a.name))
         }
         case 'check': {
-
-            return state // need to fix
+            return state.filter(user => user.age >= action.payload)
         }
         default:
             return state
