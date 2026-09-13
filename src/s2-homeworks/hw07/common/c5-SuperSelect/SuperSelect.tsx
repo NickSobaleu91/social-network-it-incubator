40df5215c0b0f8b345857c1d8889838155fe13da
Mikita Sobaleu <nick-99@tut.by>
2026-09-13 21:06:00 +0300

    feat: complete HW15

--- src/s2-homeworks/hw07/common/c5-SuperSelect/SuperSelect.tsx
+++ src/s2-homeworks/hw07/common/c5-SuperSelect/SuperSelect.tsx
@@ -31,6 +31,7 @@ const SuperSelect: React.FC<SuperSelectPropsType> = ({
         : [] // map options with key
 
     const onChangeCallback = (e: ChangeEvent<HTMLSelectElement>) => {
+        onChange?.(e)
 
         if (onChangeOption) {
             onChangeOption(Number(e.currentTarget.value))
